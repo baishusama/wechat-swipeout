174c06b1d0835370546f6df1008c091c811503d7
baishusama <baishusama@outlook.com>
2018-12-05 11:44:51 +0800

    feat: finish basic version

--- src/index.js
+++ src/index.js
@@ -1,18 +1,256 @@
+// const prefix = '__wechat-swipeout_'
+const MIN_BTN_W = 100 // (rpx)
+const WINDOW_W = wx.getSystemInfoSync().windowWidth // (px)
+
 Component({
+  // 选项
+  options: {
+    multipleSlots: true, // 使用多个 slot 时必须
+    // addGlobalClass: true
+  },
+  // 接口 (rpx)
   properties: {
-    prop: {
-      type: String,
-      value: 'index.properties'
+    /* 主要内容 */
+    // 组件显示区域的宽度，默认全屏宽度
+    width: {
+      type: Number,
+      value: 750,
+    },
+    // 组件显示区域的高度
+    height: {
+      type: Number,
+      value: 0,
+    },
+    /* 按钮相关 */
+    // （左侧）按钮总宽度
+    leftWidth: {
+      type: Number,
+      value: 0,
+    },
+    // （左侧）作为按钮的默认宽度
+    leftButtonWidth: {
+      type: Number,
+      value: 0,
+    },
+    // （左侧）按钮列表
+    leftButtons: {
+      type: Array,
+      value: null,
+    },
+    // （右侧）按钮总宽度
+    rightWidth: {
+      type: Number,
+      value: 0,
+    },
+    // （右侧）作为按钮的默认宽度
+    rightButtonWidth: {
+      type: Number,
+      value: 0,
+    },
+    // （右侧）按钮列表
+    rightButtons: {
+      type: Array,
+      value: null,
     },
   },
+  data: {
+    totalWidth: WINDOW_W, // (rpx) （包括未显示的按钮的）总宽度，默认全屏宽度
+    leftVisibleWidth: 0, // (px) 左侧可视宽度
+    rightVisibleWidth: 0, // (px) 右侧可视宽度
+    canBeOut: false, // FIXME: enhance ???
+  },
   lifetimes: {
-    attached() {
-      wx.getSystemInfo({
-        success(res) {
-          // eslint-disable-next-line no-console
-          console.log(res)
+    ready() {
+      this.initialize()
+      this.calculate()
+    },
+  },
+  methods: {
+    // 初始化宽度 FIXME: bug ??? 需要一个 setter 监听？再自动调用？
+    initialize() {
+      // type Direction = 'left' | 'right'
+      const _initialize = direction => {
+        const widthKey = `${direction}Width`
+        const buttonWidthKey = `${direction}ButtonWidth`
+        const buttonsKey = `${direction}Buttons`
+        const updateButtons = defaultWidth => {
+          let sideTotalWidth = 0
+
+          const buttons = this.data[buttonsKey].map(button => {
+            // 副作用：计算单边的总宽度
+            sideTotalWidth += button.width || defaultWidth
+
+            // 设置未知宽度按钮的宽度
+            if (!button.width) {
+              return Object.assign(
+                {},
+                button,
+                {
+                  width: defaultWidth,
+                }
+              )
+            }
+
+            return button
+          })
+
+          this.setData({
+            [buttonsKey]: buttons,
+            [widthKey]: sideTotalWidth,
+          })
         }
+
+        // 如果未传入按钮数组，那么不需要以下初始化！
+        if (!this.data[buttonsKey]) {
+          return
+        }
+
+        if (this.data[buttonWidthKey]) {
+          // case1. 如果传入 (left|right)ButtonWidth 会重新计算 (left|right)Width
+          updateButtons(this.data[buttonWidthKey])
+        } else if (this.data[widthKey]) {
+          // case2. 如果传入 (left|right)Width 会平均剩余给未设置宽度的按钮
+          let sideTotalWidth = 0
+          const widthUnknownButtons = this.data[buttonsKey].filter(button => {
+            // 副作用：计算宽度已知的按钮们占用的宽度
+            if (button.width) {
+              sideTotalWidth += button.width
+            }
+
+            return !button.width
+          })
+
+          // 如果已设置宽度按钮的总宽度超出可能，那么抛异常
+          if (
+            sideTotalWidth > this.data[widthKey] ||
+            (sideTotalWidth === this.data[widthKey] &&
+              widthUnknownButtons.length > 0)
+          ) {
+            throw new Error(
+              `You should set enough "${widthKey}" for <wechat-swipeout> component`
+            )
+          }
+
+          if (widthUnknownButtons.length > 0) {
+            // 如果存在未设置宽度的按钮，那么设置宽度和更新按钮
+            const restButtonWidth =
+              (this.data[widthKey] - sideTotalWidth) /
+              widthUnknownButtons.length
+            widthUnknownButtons.forEach(button => {
+              button.width = restButtonWidth
+            })
+            this.setData({
+              [buttonsKey]: widthUnknownButtons,
+            })
+          } else if (sideTotalWidth !== this.data[widthKey]) {
+            // 如果不存在未设置宽度的按钮，且宽度不完全匹配，那么调整宽度到正合适
+            this.setData({
+              [widthKey]: sideTotalWidth,
+            })
+          }
+        } else {
+          // case3. 如果什么都没有设置，根据最小值初始化按钮列表数据
+          updateButtons(MIN_BTN_W)
+        }
+      }
+
+      // 接口方式（非 slot 方式）
+      _initialize('left')
+      _initialize('right')
+    },
+    // 计算总宽度
+    calculate() {
+      const leftW = this.data.leftWidth || 0
+      const rightW = this.data.rightWidth || 0
+      // TODO: test to del
+      console.log(`[test] calculate, totalWidth: ${leftW + this.data.width + rightW} = ${leftW} + ${this.data.width} + ${rightW}`)
+
+      this.setData({
+        totalWidth: leftW + this.data.width + rightW,
       })
-    }
-  }
+    },
+
+    onTouchStart(e) {
+      console.log('[test] e.changedTouches[0] :', e.changedTouches[0])
+    },
+    onTouchEnd() { },
+    // FIXME: enhance ??? 节流？防抖？
+    onChange(e) {
+      const x = e.detail.x
+      console.log('[test] onChange, x :', x) // TODO: test to del
+
+      if (x < 0 && this.data.rightButtons) {
+        this.setData({
+          rightVisibleWidth: Math.min(-x, Math.floor(this.data.rightWidth / 750 * WINDOW_W))
+        })
+      } else if (x > 0 && this.data.leftButtons) {
+        this.setData({
+          rightVisibleWidth: Math.min(x, Math.floor(this.data.leftWidth / 750 * WINDOW_W))
+        })
+      } else {
+        if (this.data.leftButtons) {
+          this.setData({
+            leftVisibleWidth: 0
+          })
+        }
+        if (this.data.rightButtons) {
+          this.setData({
+            rightVisibleWidth: 0
+          })
+        }
+      }
+
+      /* 比起用 js 来 setData 直接用 css 吧
+
+      */
+    },
+    // 点击按钮时，触发 press 自定义事件
+    onButtonPress(e) {
+      const hash = e.currentTarget.dataset.hash
+      console.log('[test] onPress, hash :', hash)
+
+      const eventDetail = { hash }
+      const eventOption = {
+        bubbles: true,
+      }
+      this.triggerEvent('press', eventDetail, eventOption)
+    },
+    // (点击|释放)按钮的时候，反转背景色
+    toggleBackgroundColor(e) {
+      // TODO: test to del
+      console.log('[test] toggleBackgroundColor, e :', e)
+
+      const key = e.currentTarget.dataset.key // 决定哪侧 (left|right)Buttons
+      const hash = e.currentTarget.dataset.hash // 决定哪个按钮
+      const buttons = this.data[key].map(button => {
+        // 对匹配的按钮做背景色的变化
+        if (button.hash === hash) {
+          return Object.assign(
+            {},
+            button,
+            // 交换背景色
+            {
+              backgroundColor: button.underlayColor,
+              underlayColor: button.backgroundColor,
+            }
+          )
+        }
+
+        return button
+      })
+
+      this.setData({
+        [key]: buttons
+      })
+    },
+    // // TODO: test to del
+    // test() {
+    //   console.log('[test] ///=================')
+    //   console.log('[test] totalWidth :', this.data.totalWidth)
+    //   console.log('[test] rightButtonWidth :', this.data.rightButtonWidth)
+    //   console.log('[test] rightButtons :', this.data.rightButtons)
+    //   console.log('[test] rightWidth :', this.data.rightWidth)
+    //   console.log('[test] \\\\\\================')
+    // },
+  },
 })
--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -1,1 +1,56 @@
-Page({})
+Page({
+  /**
+   * ImoNote: 关于 left/right 要传入的 rightButtonList
+   * hash 必须唯一，由用户判断决定如何调用对应的函数
+   */
+  data: {
+    leftButtonList: [
+      {
+        hash: 'share',
+        backgroundColor: '#fff',
+        color: '#000',
+        underlayColor: '#ddd',
+        text: 'share',
+        width: 150,
+      },
+    ],
+    rightButtonList: [
+      {
+        hash: 'confirm',
+        backgroundColor: '#f00',
+        color: '#fff',
+        underlayColor: 'brown',
+        text: 'Ok',
+        width: 150,
+      },
+      {
+        hash: 'cancel',
+        backgroundColor: '#fff',
+        color: '#f00',
+        underlayColor: '#ddd',
+        text: 'x',
+        // width: 150, // undefined then will use specific or default width
+      },
+    ],
+  },
+  share() {
+    console.log('[test] share, this :', this)
+  },
+  love() {
+    console.log('[test] love, this :', this)
+  },
+  confirm() {
+    console.log('[test] confirm, this :', this)
+  },
+  cancel() {
+    console.log('[test] cancel, this :', this)
+  },
+  onSwipeoutPress({ detail }) {
+    const { hash } = detail
+
+    // 【自定义】根据 hash 执行特定方法
+    if (typeof this[hash] === 'function') {
+      this[hash]()
+    }
+  },
+})
