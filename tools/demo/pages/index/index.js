0440446bf21974e687f00ae6df416bce8df5e84c
baishusama <baishusama@outlook.com>
2018-12-07 14:11:17 +0800

    test: add icon assets, demo `iconPath`, enhance love button

--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -1,3 +1,25 @@
+let _doLoveIt = false // flag
+const _loveBtnBase = {
+  hash: 'love',
+  className: 'love-button',
+  width: 300,
+  backgroundColor: '#fff',
+} // reuse
+const _notLovedItBtn = {
+  ..._loveBtnBase,
+  iconPath: '../assets/heart.png',
+  text: 'I love it !',
+  color: '#bfbfbf',
+  underlayColor: '#ffb6c1',
+}
+const _lovedItBtn = {
+  ..._loveBtnBase,
+  iconPath: '../assets/heart-fill.png',
+  text: 'I loved it !',
+  color: '#ff69b4',
+  underlayColor: '#bfbfbf',
+}
+
 Page({
   /**
    * ImoNote: 关于 left/right 要传入的 rightButtonList
@@ -7,37 +29,33 @@ Page({
     leftButtonList: [
       {
         hash: 'share',
-        backgroundColor: '#fff',
+        className: 'share-button',
+        width: 100,
+        iconPath: '../assets/share.png',
+        // text: 'share',
         color: '#000',
-        underlayColor: '#ddd',
-        text: 'share',
-        width: 150,
+        backgroundColor: '#87ceeb',
+        underlayColor: '#00bfff',
       },
-      {
-        hash: 'love',
-        backgroundColor: 'pink',
-        color: '#fff',
-        underlayColor: 'deeppink',
-        text: 'love',
-        // width: 150,
-      },
+      { ...(_doLoveIt ? _lovedItBtn : _notLovedItBtn) }
     ],
     rightButtonList: [
       {
         hash: 'confirm',
-        backgroundColor: '#f00',
-        color: '#fff',
-        underlayColor: 'brown',
-        text: 'Ok',
         width: 150,
+        text: 'Ok',
+        color: '#fff',
+        backgroundColor: '#f00',
+        underlayColor: '#a52a2a',
       },
       {
         hash: 'cancel',
-        backgroundColor: 'gold',
-        color: '#fff',
-        underlayColor: 'orange',
-        text: 'x',
         // width: 150, // undefined then will use specific or default width
+        iconPath: '../assets/close.png',
+        // text: 'x',
+        color: '#fff',
+        backgroundColor: '#ffd700',
+        underlayColor: '#ffa500',
       },
     ],
   },
@@ -53,8 +71,17 @@ Page({
   share() {
     console.log('[test] share, this :', this)
   },
-  love() {
+  love(index) {
     console.log('[test] love, this :', this)
+
+    // 反转按钮状态
+    this._toggleLoveButton()
+    // 延时关闭（需要 index 存在）
+    if (typeof index !== 'undefined') {
+      setTimeout(() => {
+        this.selectAllComponents('.swipeout')[index].close()
+      }, 1000)
+    }
   },
   confirm() {
     console.log('[test] confirm, this :', this)
@@ -63,26 +90,35 @@ Page({
     console.log('[test] cancel, this :', this)
   },
   // 打开所有 swipeout 组件左侧
-  openAllSwipeoutLeft(){
-    const compList = this.selectAllComponents('.swipeout')
-    compList.forEach(comp => comp.openLeft())
+  openAllSwipeoutLeft() {
+    this.selectAllComponents('.swipeout').forEach(comp => comp.openLeft())
   },
   // 打开所有 swipeout 组件右侧
-  openAllSwipeoutRight(){
-    const compList = this.selectAllComponents('.swipeout')
-    compList.forEach(comp => comp.openRight())
+  openAllSwipeoutRight() {
+    this.selectAllComponents('.swipeout').forEach(comp => comp.openRight())
   },
   // 关闭（复位）所有 swipeout 组件
-  closeAllSwipeout(){
-    const compList = this.selectAllComponents('.swipeout')
-    compList.forEach(comp => comp.close())
+  closeAllSwipeout() {
+    this.selectAllComponents('.swipeout').forEach(comp => comp.close())
   },
-  onSwipeoutPress({ detail }) {
-    const { hash } = detail
+  // 当 <swipeout> 的 press 事件触发时的回调
+  onSwipeoutPress(e) {
+    const { index } = e.currentTarget.dataset
+    const { hash } = e.detail
 
     // 【自定义】根据 hash 执行特定方法
     if (typeof this[hash] === 'function') {
-      this[hash]()
+      this[hash](index)
     }
   },
+  _toggleLoveButton() {
+    _doLoveIt = !_doLoveIt
+
+    const leftButtonList = [...this.data.leftButtonList]
+    leftButtonList[1] = { ...(_doLoveIt ? _lovedItBtn : _notLovedItBtn) }
+
+    this.setData({
+      leftButtonList
+    })
+  }
 })
